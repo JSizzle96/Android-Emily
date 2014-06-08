4b3d29f956f87647ea02078b97a185084a3c86b2
Emily Lerner <Emily@Emilys-MacBook-Pro.local>
2014-06-07 23:55:52 -0400

    Made BaseGameOB

--- KillCircle/src/com/example/killcircle/BaseGameOB.java
+++ KillCircle/src/com/example/killcircle/BaseGameOB.java
@@ -1,5 +1,30 @@
 package com.example.killcircle;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 public class BaseGameOB {
 
+	int x;
+	int y;
+	int w;
+	int h;
+	Paint P;
+	
+	
+	public BaseGameOB(int x, int y, int w, int h, Paint p) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		P = p;
+	}
+	public void Draw(Canvas C)
+	{
+	
+	}
+	public void Update(long mi) {
+		
+	}
 }
--- KillCircle/src/com/example/killcircle/Square.java
+++ KillCircle/src/com/example/killcircle/Square.java
@@ -1,5 +1,17 @@
 package com.example.killcircle;
 
-public class Square {
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Square extends BaseGameOB{
+	//xywh
+	public Square(int x, int y, int w, int h, Paint p) {
+		super(x, y, w, h, p);
+		
+	}
+	public void Draw(Canvas C)
+	{
+		C.drawRect(x,y,x+w,y+h, P);
+	}
 
 }
